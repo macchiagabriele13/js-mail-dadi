e53053b925494350cddcf48bbd1d5fc4b3f53911
macchiagabriele13 <macchia_gabriele@libero.it>
2022-10-11 16:39:08 +0200

    modifiche es 1, ma non riesco a capire passaggio finale if

---
+++ assets/js/main.js
@@ -0,0 +1,78 @@
+/* Mail */
+
+/* Chiedi all’utente la sua email, */
+const insertMail = prompt("Inserisci la tua email");
+console.log(insertMail);
+
+
+
+
+/* Creo una lista di mail valide */
+
+const validMail = [
+    "sr.lele13@libero.it" ,
+    "macchia_gabriele13@gmail.com",
+    "gabrielemacchia@libero.it",
+    "ziopino@gmail.com",
+    "provaprova@gmail.com",
+]
+
+let message = 'ERRORE';
+console.log(message);
+
+for (let i = 0; i < validMail.length; i++) {
+    const validMailElement = validMail[i];
+    console.log(validMailElement);
+    
+    if (insertMail === validMailElement) {
+        message = "Benvenuto";
+        console.log(message);
+    }
+}
+
+let userMessage = document.querySelector("h1");
+
+if (message = "Benvenuto") {
+
+    userMessage.innerHTML = "Benvenuto"
+    
+} else {
+userMessage.innerHTML = "Ops, qualcosa è andato storto"
+}
+
+
+
+/* controlla che sia nella lista di chi può accedere, */ 
+
+/* stampa un messaggio appropriato sull’esito del controllo. */
+
+
+
+
+
+
+
+
+
+
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto. */
+
+
+
+
+
+/* Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve? */
+
+
+
+/* Consigli del giorno:
+scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+
+javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+
+si ma noi cosa vogliamo fare?
+
+torniamo a scrivere in italiano
+
+proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano" */
